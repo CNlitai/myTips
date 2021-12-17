368dcc908cb9d12d841f2069ca33fa8e5fa50456
CN-Litai <2083645366@qq.com>
2021-12-17 10:11:40 +0800

    fix

--- download/downloadfile.js
+++ download/downloadfile.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-12-13 15:06:39
- * @LastEditTime: 2021-12-13 15:16:14
+ * @LastEditTime: 2021-12-17 10:11:29
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /myTips/downloadfile.js
